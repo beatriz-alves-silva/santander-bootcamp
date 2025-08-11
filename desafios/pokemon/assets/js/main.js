75d79404901384b3fc0d32662d24749507a245a7
Beatriz Alves <beatriz.alves.silva.cc@gmail.com>
2025-08-10 22:00:04 -0300

    feat: cria JS para consumo de API e alteração no estilo da pagina

---
+++ desafios/pokemon/assets/js/main.js
@@ -0,0 +1,68 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+
+const maxRecords = 151
+const limit = 12
+let offset = 0;
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon ${pokemon.type}">
+            <div class="number-div">
+                <span class="number">#${pokemon.number}</span>
+            </div>
+
+            <span class="name">${pokemon.name}</span>
+
+            <div class="image">
+                <img src="${pokemon.photo}"
+                    alt="${pokemon.name}">
+            </div>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+            </div>
+        </li>
+    `
+}
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    const qtdRecordsWithNexPage = offset + limit
+
+    if (qtdRecordsWithNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
+})
+
+pokemonList.addEventListener('click', (event) => {
+    const cardClicado = event.target.closest('.pokemon');
+    
+    if (!cardClicado) {
+        return;
+    }
+
+    const cardAtivo = document.querySelector('.pokemon.image-view');
+
+    if (cardAtivo && cardAtivo !== cardClicado) {
+        cardAtivo.classList.remove('image-view');
+    }
+
+    cardClicado.classList.toggle('image-view');
+});
